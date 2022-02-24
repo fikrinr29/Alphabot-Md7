af9c9e0d4ef5928499ef2ba719cbe22426562d07
fikrinr29 <96033914+fikrinr29@users.noreply.github.com>
2022-02-24 13:29:50 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["6285845485792","6285845485792","6285345047847"] //ganti agar fitur owner bisa di gunakan
 global.packname = '© AssistenBOTZ-Mdོ' //sticker wm
-global.author = 'I hope you're fine' //sticker wm
+global.author = 'I hope you fine' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
